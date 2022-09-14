0d95d2e55c9e8e8b4ff9594bd2bcb0ea1b4829e8
Carlos Tamayo <c.tamayo1974@gmail,com>
2022-09-13 18:19:14 -0600

    Build routing on navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import {AppComponent} from './app.component';
+import { AboutComponent} from './about/about.component';
+import { CreenciasComponent } from './creencias/creencias.component';
+import { ContribuirComponent} from './contribuir/contribuir.component';
+
+const routes: Routes = [
+  {path: 'home', component: AppComponent},
+  {path: 'about', component:AboutComponent},
+  {path: 'creencias', component:CreenciasComponent},
+  {path: 'contribute', component: ContribuirComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
